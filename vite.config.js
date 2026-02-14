fa46b931e5c3ae8112d77d2872bb3bda6ac45cf5
Andrew Mo <andrewmo@Andrews-Laptop.local>
2026-02-14 11:47:04 -0800

    gh pages ready

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/andrew-hannah-valentine2026/'
+  base: '/valentines2026/'
 })
